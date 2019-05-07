4ed620e4853f67a987aff1cee75a8026663d90c5
Arthur Pinheiro <arthurpinheiro@MacBook-Pro-de-Arthur.local>
2019-05-07 20:48:55 -0300

    teste branch

---
+++ portal/includes/config_arthur.php
@@ -0,0 +1,15 @@
+<?php
+    @session_start();
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    $dbname = 'maniza';
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    //teste
+
+    $base = 'http://localhost/painel_controle/';
+    $path = '/Applications/MAMP/htdocs/maniza/';
+?>
