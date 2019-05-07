5f099cbc39c67090d986dc9914ee1485b273e425
robsonrochacxs <robsonrocha.cxs@gmail.com>
2019-05-07 20:57:18 -0300

    teste

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+<div class="limpa"></div>
+
+<div id="rodape">
+    <div class='container'>
+        <div class="endereco">
+            <h2>Fale conosco</h2>
+            <a href = "mailto: robson@maniza.com.br"><i class="fas fa-at"></i> robson@maniza.com.br</a>
+            <a href="tel:+5554981254188"><i class="fab fa-whatsapp"></i> (54) 98125.4188</a><br>
+            <p><i class="fas fa-map-marker-alt"></i> Rua Nedem Adelar Chedid, 68 - 101</p>
+            <p>95032-794 - N. S. da Saúde - Caxias do Sul-RS</p>
+        </div>
+
+        <div class="limpa"></div>
+        
+        <div class="links">
+            <h2>Curta nossas páginas</h2>
+            <ul>
+                <li><a href="https://www.linkedin.com/in/robson-rocha-506b7257/" target="blank"><i class="fab fa-linkedin"></i> Linkedin</a></li>
+            </ul>
+            <ul>
+                <li><a href="https://www.instagram.com/" target="blank"><i class="fab fa-instagram"></i> Instagram</a></li>
+            </ul>
+        </div>
+
+        <div class="limpa"></div>
+        
+        <div class="links">
+            <h2>Normas de SST</h2>
+            <ul>
+                <li><a href="https://enit.trabalho.gov.br/portal/index.php/seguranca-e-saude-no-trabalho/sst-menu/sst-normatizacao/sst-nr-portugues?view=default" target="blank"><i class="fas fa-gavel"></i> Normas MTE</a></li>
+            </ul>
+            <ul>
+                <li><a href="http://www.fundacentro.gov.br/biblioteca/normas-de-higiene-ocupacional" target="blank"><i class="fas fa-hard-hat"></i> NHO Fundacentro</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Robson Rocha</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/responsive.css">
+</head>
+
+<body>
+<div id="topo">
+    <div class='container'>
+        <div class="logo">
+            <img src="assets/img/logo.png" alt="Logo Maniza">
+        </div>
+        <div class="titulo">
+            <h1>MANIZA SOLUÇÕES TÉCNICAS</h1>
+            <h2>Assessoria, Projetos e Documentação Técnica</h2>
+        </div>
+        <div class="restrito">
+            <ul>
+                <li><a href="portal/login.php" target="blank"><i class="fas fa-lock"></i> Restrito</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="limpa"></div>
